e5b07846abf6224aa5da23c1e8d5a31d2c78cdaf
yksu <47516117+yksu@users.noreply.github.com>
2023-05-23 11:12:34 +0200

    Update partial_view.py

--- partial_view.py
+++ partial_view.py
@@ -3,11 +3,11 @@ import torch
 import numpy as np
 import open3d as o3d
 import util
+from util import set_Camera_Rasterizer
 import pytorch3d.renderer
 from pytorch3d.renderer import (
-    PerspectiveCameras, look_at_view_transform,
-    RasterizationSettings, BlendParams,
-    MeshRenderer, MeshRasterizer, SoftPhongShader
+    look_at_view_transform, BlendParams,
+    MeshRenderer, SoftPhongShader
 )
 import pytorch3d
 import matplotlib.pyplot as plt
@@ -19,198 +19,110 @@ import warnings
 warnings.filterwarnings('ignore')
 
 class Partial_views:
+	### Part 1 initialization
+	def __init__(self, pose_axisang=None, betas=None):
+		self.setRef(gender="M")
+		self.setMesh(pose_axisang=pose_axisang,betas=betas)
+
+		### Set face location for mesh and ref_mesh
+		self.face_location_attr_scan        =    self.mesh.verts_packed()[self.mesh.faces_packed()].reshape((13776, 3,-1))
+		self.face_location_attr_scan_normal =    self.mesh.verts_normals_packed()[self.mesh.faces_packed()].reshape((13776, 3,-1))
+		self.face_location_attr_ref        = self.mesh_ref.verts_packed()[self.mesh.faces_packed()].reshape((13776, 3,-1))
+		self.face_location_attr_ref_normal = self.mesh_ref.verts_normals_packed()[self.mesh.faces_packed()].reshape((13776, 3,-1))
 	
-	def __init__(self, pose_axisang, betas):
-		self.setSMPL()
-		self.setTestSMPL(pose_axisang,betas)
-		self.setRefMesh()
-		self.setMesh()
-		self.setFaceLocationMesh()
-		self.setFaceLocationRefMesh()
-		
-	
-	def setSMPL(self):
+	def setRef(self,gender="M"):
 		''' 
-    	Set the SMPL object from models by default use the male object
-    	'''	
-		self.smpl = util.SMPL_path(gender="M").get_smpl()
+		Set the reference SMPL object and the reference Mesh object.
+		Args:
+			gender (str) : gender of the object
+		'''	
+		if gender == "M":
+			self.smpl = util.SMPL_path(gender="M").get_smpl()
+		elif gender == "F":
+			self.smpl = util.SMPL_path(gender="F").get_smpl()
+		else:
+			self.smpl = util.SMPL_path(gender="N").get_smpl()
 
-	def setTestSMPL(self,pose_axisang,betas):
-		''' 
-    	Set the SMPL object
-    
-    	Args:
-    	    pose_axisang (torch): (batch_size,72) 
-    	    betas (torch): (batch_size,300) 
-    	'''	
-		test_smpl = SMPL_Layer(model_root="./models")
-		test_vert = test_smpl.smpl_data["v_posed"]
-		
-		test_vert = np.array(test_vert)
-		no_input_para=True
-
-		if no_input_para:
-			self.vertices = torch.from_numpy(test_vert).float().cpu()[None,:]
-		else :
-			self.vertices,_ = test_smpl(pose_axisang,betas)		
-
-
-	def setMesh(self,mesh_vert=None):
-		''' 
-
-    	Set the Mesh object
-    
-    	'''	
-		# 6890 vertices, 13776 faces
-
-		self.mesh     = Meshes(self.vertices, self.ref_faces)
-
-
-	def setRefMesh(self):
-		''' 
-    	Set the Mesh object for reference
-	    
-    	'''	
 		self.ref_faces    = self.smpl.faces.astype('float32')
 		self.ref_vertices = self.smpl.shapedirs
 		self.ref_faces	  = torch.from_numpy(self.ref_faces).float().cpu()[None,:]
 		self.ref_vertices =  self.ref_vertices.float().cpu()[None,:]
 		self.mesh_ref = Meshes(self.ref_vertices, self.ref_faces)
-
-	def setFaceLocationMesh(self):
-		''' 
-    	Set the face location of the Mesh object
-    
-    	
-    	'''	
-		self.face_location_attr_scan        =    self.mesh.verts_packed()[self.mesh.faces_packed()].reshape((13776, 3,-1))
-		self.face_location_attr_scan_normal =    self.mesh.verts_normals_packed()[self.mesh.faces_packed()].reshape((13776, 3,-1))
 		
-	def setFaceLocationRefMesh(self):
+	def setSMPL(self,pose_axisang=None,betas=None):
 		''' 
-    	Set the face location of the reference Mesh object
-     
-    	'''	
-		self.face_location_attr_ref        = self.mesh_ref.verts_packed()[self.mesh.faces_packed()].reshape((13776, 3,-1))
-		self.face_location_attr_ref_normal = self.mesh_ref.verts_normals_packed()[self.mesh.faces_packed()].reshape((13776, 3,-1))
-	
-	def show_image(self):
+		Set the SMPL object from models by default use the male object
+		Args:
+			pose_axisang (torch): (batch_size,72) 
+			betas (torch): (batch_size,300) 
+		'''	
+		
+		self.test_smpl = SMPL_Layer(model_root="./models")
+		test_vert = self.test_smpl.smpl_data["v_posed"]
+		
+		test_vert = np.array(test_vert)
+		
+		if pose_axisang==None:
+			self.vertices = torch.from_numpy(test_vert).float().cpu()[None,:]
+		else :
+			self.vertices,_ = self.test_smpl(pose_axisang,betas)		
+
+		self.mesh     = Meshes(self.vertices, self.ref_faces)
+
+	def updateMesh(self,pose_axisang,betas=None):
+		''' 
+		Update the mesh by pose_axisang and betas
+		Args:
+			pose_axisang (torch): (batch_size,72) 
+			betas (torch): (batch_size,300) 
+		'''	
+		self.vertices,_ = self.test_smpl(pose_axisang,betas)
+		self.mesh       = Meshes(self.vertices, self.ref_faces)	
+
+	### Part 2: depth map creating and 2D image showing
+	def show_image(self,show=True,save=False):
 		''' 
     	Show the 2D depth map and save the image
     
-    	
     	'''	
-		self.depth_map()
+		self.setDepth_map(H=None,W=None,R=None,T=None,K=None)
 
 		depth = self.depth
 		plt.imshow(self.depth[0, :,:, 0].cpu().numpy())
-		#plt.show()
-		plt.savefig('2D_object.png')
+		if show:
+			plt.show()
+		if save:
+			plt.savefig('2D_object.png')
 		plt.close()
 
 		# get partial pointcloud from rendering results
 		# get correct order in data
 
-	def set_intrinsic(self, R=None, T=None, intrinsics=None):
-		''' 
-    	Set the intrinsic parameter for the camera
-    
-    	Args:
-    	    R (np.ndarray): (3,) array storing the rotation angle
-    	    T (np.ndarray): (3,) array storing the translation vector
-    	    intrinsics (np.ndarray): (4,) array storing the camaera calibration matrix
-    	'''
-		if R == None:
-			self.R = torch.tensor([[
-					[-1.,  0.,  0.],
-					[ 0.,  1.,  0.],
-					[ 0.,  0., -1.]
-					]]).cpu()
-		else:
-			r = Rotation.from_euler('zyx', R, degrees=True)
-			self.R = r.as_matrix()
-		if T == None:
-			self.T = torch.tensor([[-0.0000, 0.2, 2.7000]]).cpu()
-		else:
-			self.T = T
-		# BEHAVE: Kinect azure depth camera 0 parameters
-		if intrinsics == None:
-			intrinsics = [502.9671325683594,503.04168701171875,322.229736328125,329.3377685546875]
-			fx = intrinsics[0]
-			fy = intrinsics[1]
-			cx = intrinsics[2]
-			cy = intrinsics[3]
-			self.intrinsics = torch.tensor([[
-							[fx, 0.0, cx, 0.0],
-							[0.0, fy, cy, 0.0],
-							[0.0, 0.0, 0.0, 1.0],
-							[0.0, 0.0, 1.0, 0.0]]]).cpu()
-		else:		
-			fx = intrinsics[0]
-			fy = intrinsics[1]
-			cx = intrinsics[2]
-			cy = intrinsics[3]
-			self.intrinsics = torch.tensor([[
-							[fx, 0.0, cx, 0.0],
-							[0.0, fy, cy, 0.0],
-							[0.0, 0.0, 0.0, 1.0],
-							[0.0, 0.0, 1.0, 0.0]]]).cpu()
-
-
-	def set_Camera(self):
-		''' 
-    	Set the the parameter of the camera
-    
-		'''	
-		self.image_size_height = 576
-		self.image_size_width = 640
-		self.image_size = torch.tensor([self.image_size_height, self.image_size_width]).unsqueeze(0).cpu()
-		self.cameras_front = PerspectiveCameras(R=self.R, T=self.T, in_ndc=False, K=self.intrinsics, image_size=self.image_size).cpu()
-
-	def setKinect(self):
-		self.raster_settings_kinect = RasterizationSettings(
-			image_size=(self.image_size_height, self.image_size_width),
-			blur_radius=0,
-			faces_per_pixel=1,  # number of faces to save per pixel
-			bin_size=0
-			)
-
-	def setRasterizer(self):
-		self.rasterizer = MeshRasterizer(
-			cameras=self.cameras_front, 
-			raster_settings=self.raster_settings_kinect
-			)
-	def depth_map(self):
-		mesh = self.mesh
+	def setDepth_map(self,H=None,W=None,R=None,T=None,K=None):
 		
-		T = torch.tensor([[-0.0000, 1.2, 2.7000]]).cpu()
-		self.set_intrinsic(R=None,T=T)
-		self.set_Camera()
-		self.setKinect()
-		self.setRasterizer()
+		self.rasterizer = set_Camera_Rasterizer(H,W,R,T,K)
 
-		fragments = self.rasterizer(mesh)
+		fragments = self.rasterizer(self.mesh)
 
 		self.depth = fragments.zbuf
 		self.pix_to_face = fragments.pix_to_face
 		self.barycentric = fragments.bary_coords
 
-	def setPixelScan(self):
-		self.pixel_location_scan = interpolate_face_attributes( self.pix_to_face, self.barycentric, self.face_location_attr_scan)
-		self.pixel_normal_scan   = interpolate_face_attributes( self.pix_to_face, self.barycentric, self.face_location_attr_scan_normal)
 
-	def setPixelRef(self):
-		self.pixel_location_ref = interpolate_face_attributes(self.pix_to_face, self.barycentric, self.face_location_attr_ref)
-		self.pixel_normal_ref = interpolate_face_attributes  (self.pix_to_face, self.barycentric, self.face_location_attr_ref_normal)
-
+		
+	### Part 3: recover 3D partial view from the 2D depth map
 	def recover3D(self):
 		''' 
     	Recover the partial view from the 2D depth map
 	    
     	'''	
 		#pixel_vals: tensor of shape (N, H, W, K, D) giving the interpolated value of the face attribute for each pixel.
-		self.setPixelScan()
-		self.setPixelRef()
+		self.pixel_location_scan = interpolate_face_attributes( self.pix_to_face, self.barycentric, self.face_location_attr_scan)
+		self.pixel_normal_scan   = interpolate_face_attributes( self.pix_to_face, self.barycentric, self.face_location_attr_scan_normal)
+
+		self.pixel_location_ref = interpolate_face_attributes(self.pix_to_face, self.barycentric, self.face_location_attr_ref)
+		self.pixel_normal_ref = interpolate_face_attributes  (self.pix_to_face, self.barycentric, self.face_location_attr_ref_normal)
+
 		
 		points, idx, counts = torch.unique(self.pixel_location_scan.reshape(-1, 3), 
 			sorted=True, return_inverse=True, return_counts=True, dim=0)
@@ -227,9 +139,8 @@ class Partial_views:
 		nonZeroRows = torch.abs(self.pixel_normal_ref.reshape(-1, 3)[first_indicies]).sum(dim=1) > 0
 		zero_idx = (nonZeroRows == False).nonzero(as_tuple=True)[0]
 
-		pcd = o3d.geometry.PointCloud()
+		self.pcd = o3d.geometry.PointCloud()
+		self.pcd.points = o3d.utility.Vector3dVector(depth_points)
 		
-		pcd.points = o3d.utility.Vector3dVector(depth_points)
-		self.pcd = pcd
-		self.depth_points = depth_points
-		o3d.visualization.draw_geometries([pcd])
+	def show_3D_Partial_View(self):
+		o3d.visualization.draw_geometries([self.pcd])
